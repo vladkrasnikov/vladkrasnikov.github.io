c9eb3c4fa34d00e7fc7437b4dc826dc3fb630dc5
Vladyslav Krasnikov <krasnikov.vladislav71@gmail.com>
2022-11-08 10:24:37 +0200

    format code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent },
-{ path: 'about', component: AboutComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
